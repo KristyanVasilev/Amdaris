4cb2507481fec83f8221a973e7e834fa25c4291d
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-08 14:29:03 +0200

    Create enums for Game

---
+++ BookShop/Data/Hobbies/Enums/GameType.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Hobbies.Enums
+{
+    public enum GameType
+    {
+        Board = 1,
+        Strategy = 2,
+        Adventure = 3,
+        RolePlay = 4,
+        Party = 5,
+        Card = 6,
+    }
+}
