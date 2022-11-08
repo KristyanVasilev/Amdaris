f795e9392bbdae475509c1317fae77ebd72fdc3b
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-08 12:40:51 +0200

    Rename BagPack to Bagpack

--- BookShop/Data/ForSchool/Bags/Bagpack.cs
+++ BookShop/Data/ForSchool/Bags/Bagpack.cs
@@ -1,8 +1,8 @@
 ﻿namespace Data.ForSchool.Bags
 {
-    public class BagPack : Bag
+    public class Bagpack : Bag
     {
-        public BagPack(string name, decimal price, string color, string manufacturer, int capacity, Type type)
+        public Bagpack(string name, decimal price, string color, string manufacturer, int capacity, Type type)
             : base(name, price, color, manufacturer, capacity, type)
         {
         }
