a9ff0af14a26b724e97aebab4932bae9bd42ecf7
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-08 12:39:39 +0200

    Created Bagpack

---
+++ BookShop/Data/ForSchool/Bags/Bagpack.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.ForSchool.Bags
+{
+    public class BagPack : Bag
+    {
+        public BagPack(string name, decimal price, string color, string manufacturer, int capacity, Type type)
+            : base(name, price, color, manufacturer, capacity, type)
+        {
+        }
+    }
+}
