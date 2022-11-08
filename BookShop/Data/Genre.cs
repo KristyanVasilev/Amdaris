ddf432888113e781982f4e44ca11c8fbf3f636a8
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-08 11:19:04 +0200

    Rename Category to Genre

---
+++ BookShop/Data/Genre.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Genre
+    {
+        private string name;
+
+        public Genre(string name)
+        {
+            this.Books = new List<Book>();
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty!");
+                }
+                name = value; 
+            }
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
