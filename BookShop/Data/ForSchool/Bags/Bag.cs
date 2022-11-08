f315a39f2c75a340f0bf0c0b20575aeea340ffb4
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-08 12:32:05 +0200

    Created Bag

---
+++ BookShop/Data/ForSchool/Bags/Bag.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Data.ForSchool.Bags
+{
+    public abstract class Bag
+    {
+        private string name;
+        private decimal price;
+        private string color;
+        private string manufacturer;
+
+        public Bag(string name, decimal price, string color, string manufacturer, Type type)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Color = color;
+            this.Manufacturer = manufacturer;
+            this.Type = type;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty!");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price cannot be negative or equal to zero!");
+                }
+                price = value;
+            }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Color cannot be null or empty!");
+                }
+                color = value;
+            }
+        }
+
+        public string Manufacturer
+        {
+            get { return manufacturer; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Manufacturer cannot be null or empty!");
+                }
+                manufacturer = value;
+            }
+        }
+
+        public Type Type { get; private set; }
+    }
+}
