115767197ae4604eb843ae6c2d9c2f8fa9588e47
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-08 14:29:11 +0200

    Create Game

---
+++ BookShop/Data/Hobbies/Game.cs
@@ -0,0 +1,75 @@
+﻿using Data.Hobbies.Enums;
+
+namespace Data.Hobbies
+{
+    public abstract class Game
+    {
+        private string name;
+        private decimal price;
+        private string manufacturer;
+        private string description;
+
+        public Game(string name, decimal price, string manufacturer, string description, GameType gameType)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Manufacturer = manufacturer;
+            this.Description = description;
+            this.GameType = gameType;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty!");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price cannot be negative or equal to zero!");
+                }
+                price = value;
+            }
+        }
+
+        public string Manufacturer
+        {
+            get { return manufacturer; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Manufacturer cannot be null or empty!");
+                }
+                manufacturer = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Description cannot be null or empty!");
+                }
+                description = value;
+            }
+        }
+
+        public GameType GameType { get; set; }
+    }
+}
