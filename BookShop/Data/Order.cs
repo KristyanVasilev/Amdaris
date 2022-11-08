f4f878f5682d5b17c7fafd823512dabd8f4b7483
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-08 15:59:07 +0200

    Create Order

---
+++ BookShop/Data/Order.cs
@@ -0,0 +1,33 @@
+﻿namespace Data
+{
+    public class Order
+    {
+        private int id;
+
+        public Order(int id, int userId)
+        {
+            this.Id = id;
+            this.UserId = userId;
+            this.Products = new List<Product>();
+        }
+
+        public int Id
+        {
+            get { return id; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Id cannot be negative or equal to zero!");
+                }
+                id = value;
+            }
+        }
+
+        public int UserId {get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public bool IsCompleted { get; private set; }
+    }
+}
