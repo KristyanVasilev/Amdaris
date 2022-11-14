474ca7eb34d337b588cb4670272554baf9dd377c
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-14 17:08:07 +0200

    Create ProductNotFoundException

---
+++ BookShop/BookShop.Infrastructure/CustomExceptions/ProductNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShop.Infrastructure.CustomExceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException(string? message)
+        : base(message)
+        {
+        }
+    }
+}
