eafddb2b4b789fc8be07433664836b9848e7534b
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-14 14:10:47 +0200

    Update Chess Puzzle and Boardgame constructor

--- BookShop/Data/Hobbies/BoardGame.cs
+++ BookShop/Data/Hobbies/BoardGame.cs
@@ -7,8 +7,7 @@
         public BoardGame(int id, decimal price, string name, string manufacturer, string description, GameType gameType)
             : base(id, price, name, manufacturer, description, gameType)
         {
-            gameType = GameType.Board;
-
+            base.GameType = GameType.Board;
         }
     }
 }
