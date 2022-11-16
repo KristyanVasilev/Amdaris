bc9ffd9bbd7c1f439cbb931882899bf853aeb4a7
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-16 13:29:13 +0200

    Refactored Order

--- BookShop/Data/Order.cs
+++ BookShop/Data/Order.cs
@@ -2,27 +2,12 @@
 {
     public class Order
     {
-        private int id;
-
-        public Order(int id, int userId)
+        public Order()
         {
-            this.Id = id;
-            this.UserId = userId;
             this.Products = new List<Product>();
         }
 
-        public int Id
-        {
-            get { return id; }
-            private set
-            {
-                if (value <= 0)
-                {
-                    throw new ArgumentException("Id cannot be negative or equal to zero!");
-                }
-                id = value;
-            }
-        }
+        public int Id { get; set; }
 
         public int UserId {get; private set; }
 
