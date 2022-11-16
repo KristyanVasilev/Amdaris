717e9e171efb78bdeee83cf396e9d3b52cb35c69
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-16 13:31:28 +0200

    Refactored Product and Genre

--- BookShop/Data/Publications/Genre.cs
+++ BookShop/Data/Publications/Genre.cs
@@ -2,24 +2,11 @@
 {
     public class Genre
     {
-        private string name;
-
         public Genre(string name)
         {
             Name = name;
         }
 
-        public string Name
-        {
-            get { return name; }
-            private set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                {
-                    throw new ArgumentException("Name cannot be null or empty!");
-                }
-                name = value;
-            }
-        }
+        public string Name { get; set; } = null!;
     }
 }
--- BookShop/Data/Publications/Publication.cs
+++ BookShop/Data/Publications/Publication.cs
@@ -4,51 +4,17 @@
 
     public class Publication : Product
     {
-        private string author;
-        private int pageCount;
+        public string Author { get; set; } = null!;
 
-        public Publication(int id, decimal price, string name, string author, string description, int pageCount, Genre genre)
-            : base(id, price, name)
-        {
-            this.Author = author;
-            this.Description = description;
-            this.PageCount = pageCount;
-            this.Genre = genre;
-        }
-
-        public string Author
-        {
-            get { return author; }
-            private set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                {
-                    throw new ArgumentException("Author cannot be null or empty!");
-                }
-                author = value;
-            }
-        }
-
-        public int PageCount
-        {
-            get { return pageCount; }
-            private set
-            {
-                if (value <= 0)
-                {
-                    throw new ArgumentException("Page count cannot be negative or equal to zero!");
-                }
-                pageCount = value;
-            }
-        }
+        public int PageCount { get; set; }
 
         public double Rating { get; set; } = 0;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = null!;
 
-        public PublicationType PublicationType { get; private set; }
+        public PublicationType PublicationType { get; set; }
 
-        public Genre Genre { get; private set; }
+        public Genre Genre { get; set; } = null!;
 
         public override string ToString()
         {
