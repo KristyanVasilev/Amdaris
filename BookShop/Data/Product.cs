0d873a7663d5e6a51c1b83ba698c5b1ebf389205
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-16 13:28:24 +0200

    Refactored Product

--- BookShop/Data/Product.cs
+++ BookShop/Data/Product.cs
@@ -4,60 +4,16 @@
 
     public abstract class Product
     {
-        private int id;
-        private decimal price;
-        private string name;
+        public int Id { get; set; }
 
-        public Product(int id, decimal price, string name)
-        {
-            this.Id = id;
-            this.Price = price;
-            this.Name = name;
-        }
+        public decimal Price { get; set; }
 
-        public int Id
-        {
-            get { return id; }
-            set
-            {
-                if (value <= 0)
-                {
-                    throw new ArgumentException("Id cannot be negative or equal to zero!");
-                }
-                id = value;
-            }
-        }
-
-        public decimal Price
-        {
-            get { return price; }
-            private set
-            {
-                if (value <= 0)
-                {
-                    throw new ArgumentException("Price cannot be negative or equal to zero!");
-                }
-                price = value;
-            }
-        }
-
-        public string Name
-        {
-            get { return name; }
-            private set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                {
-                    throw new ArgumentException("Name cannot be null or empty!");
-                }
-                name = value;
-            }
-        }
+        public string Name { get; set; } = null!;
 
         public override string ToString()
         {
             var sb = new StringBuilder();
-            sb.AppendLine($"Product {this.Name} has Id - {this.id} and cost {this.Price}$.");
+            sb.AppendLine($"Product {this.Name} has Id - {this.Id} and cost {this.Price}$.");
             return sb.ToString();
         }
     }
