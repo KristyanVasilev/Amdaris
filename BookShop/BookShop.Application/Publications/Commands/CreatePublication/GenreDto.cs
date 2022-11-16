e98dea725ca8246895388d13606a7601541189e6
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-16 12:45:35 +0200

    Create GenreDto

---
+++ BookShop/BookShop.Application/Publications/Commands/CreatePublication/GenreDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BookShop.Application.Publications.Commands.CreatePublication
+{
+    using BookShop.Data.Publications;
+
+    public class GenreDto : Genre
+    {
+        public GenreDto(string name)
+            : base(name)
+        {
+        }
+    }
+}
