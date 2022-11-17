2750273a5ec1c1120df56ceb40d5ae5ceeaf0c73
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-17 12:27:57 +0200

    Create IPublicationService and PublicationService

---
+++ BookShop/BookShop.Infrastructure/Contracts/IPublicationService.cs
@@ -0,0 +1,12 @@
+﻿namespace BookShop.Infrastructure.Contracts
+{
+    using BookShop.Data;
+
+    public interface IPublicationService
+    {
+        string AddBook(int id, decimal price, string name, string author,
+            int pageCount, double rating, string description, string genre);
+
+        Product GetBook(int id);
+    }
+}
