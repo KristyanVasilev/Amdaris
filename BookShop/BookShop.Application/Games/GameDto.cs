fbcc84b702db725916fc3966cfb5a89e8d7bc69c
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-24 16:35:16 +0200

    Create GameDto

---
+++ BookShop/BookShop.Application/Games/GameDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Application.Games
+{
+    public class GameDto : BaseProductDto
+    {
+        public string Manufacturer { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string GameType { get; set; } = null!;
+    }
+}
