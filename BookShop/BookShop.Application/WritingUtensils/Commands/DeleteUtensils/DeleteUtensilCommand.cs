80c4138024b37efe4571141d838ad4c9ffb94425
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-24 16:46:18 +0200

    Create Delete Utensil Command and Handler

---
+++ BookShop/BookShop.Application/WritingUtensils/Commands/DeleteUtensils/DeleteUtensilCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.Application.WritingUtensils.Commands.DeleteUtensils
+{
+    using MediatR;
+
+    public class DeleteUtensilCommand : IRequest<string>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ BookShop/BookShop.Application/WritingUtensils/Commands/DeleteUtensils/DeleteUtensilHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace BookShop.Application.WritingUtensils.Commands.DeleteUtensils
+{
+    using BookShop.Application.Contracts;
+    using MediatR;
+
+    public class DeleteUtensilHandler : IRequestHandler<DeleteUtensilCommand, string>
+    {
+        private readonly IWritingUtensilsRepository repository;
+
+        public DeleteUtensilHandler(IWritingUtensilsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public Task<string> Handle(DeleteUtensilCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(this.repository.DeleteUtensil(request.Id));
+        }
+    }
+}
