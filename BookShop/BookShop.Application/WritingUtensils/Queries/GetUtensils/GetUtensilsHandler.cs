478eae426ffa1f38b70cc4ac71fa44850ce0dde8
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-24 17:00:14 +0200

    Create Get all Utensils Command and Handler

---
+++ BookShop/BookShop.Application/WritingUtensils/Queries/GetUtensils/GetUtensilsHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace BookShop.Application.WritingUtensils.Queries.GetUtensils
+{
+    using BookShop.Application.Contracts;
+    using MediatR;
+
+    public class GetUtensilsHandler : IRequestHandler<GetUtensilsQuery, IEnumerable<WritingUtensilDto>>
+    {
+        private readonly IWritingUtensilsRepository repository;
+
+        public GetUtensilsHandler(IWritingUtensilsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public Task<IEnumerable<WritingUtensilDto>> Handle(GetUtensilsQuery request, CancellationToken cancellationToken)
+        {
+            var result = this.repository.GetUtensils().Select(utensil => new WritingUtensilDto
+            {
+                Id = utensil.Id,
+                Name = utensil.Name,
+                Price = utensil.Price,
+                Color = utensil.Color,
+                Manufacturer = utensil.Manufacturer,
+                WritingUtensilsType = utensil.WritingUtensilsType.Name,
+            });
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ BookShop/BookShop.Application/WritingUtensils/Queries/GetUtensils/GetUtensilsQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Application.WritingUtensils.Queries.GetUtensils
+{
+    using MediatR;
+
+    public class GetUtensilsQuery : IRequest<IEnumerable<WritingUtensilDto>>
+    {
+    }
+}
