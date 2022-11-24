4939cb46045571060ce644c65a00097b007f9c5f
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-24 15:27:56 +0200

    Create BagDto

---
+++ BookShop/BookShop.Application/Bags/BagDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BookShop.Application.Bags
+{
+    public class BagDto : BaseProductDto
+    {
+        public string Color { get; set; } = null!;
+
+        public string Manufacturer { get; set; } = null!;
+
+        public int Capacity { get; set; }
+
+        public string BagType { get; set; } = null!;
+
+        public string Gender { get; set; } = null!;
+    }
+}
