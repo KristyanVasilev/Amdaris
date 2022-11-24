efdc954fa176d989c9bf37a34915851cf0e4454e
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-24 16:54:07 +0200

    Create WritingUtensilDto

---
+++ BookShop/BookShop.Application/WritingUtensils/WritingUtensilDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Application.WritingUtensils
+{
+    public class WritingUtensilDto : BaseProductDto
+    {
+        public string Color { get; set; } = null!;
+
+        public string Manufacturer { get; set; } = null!;
+
+        public string WritingUtensilsType { get; set; } = null!;
+    }
+}
