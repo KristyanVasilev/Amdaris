ea35921640c9dfbdf6e6faf7f67eebdb68629c35
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-24 15:28:24 +0200

    Delete Bag Command and Handler

---
+++ BookShop/BookShop.Application/Bags/Commands/DeleteBag/DeleteBagCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.Application.Bags.Commands.DeleteBag
+{
+    using MediatR;
+
+    public class DeleteBagCommand : IRequest<string>
+    {
+        public int Id { get; set; }
+    }
+}
