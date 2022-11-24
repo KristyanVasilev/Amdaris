0295b609c177de4446cfcd138fd8afa8258893e0
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-24 17:00:28 +0200

    Update Program

--- BookShop/BookShop/Program.cs
+++ BookShop/BookShop/Program.cs
@@ -1,14 +1,23 @@
 ﻿namespace BookShop
 {
     using BookShop.Application.Bags.Commands.CreateBag;
+    using BookShop.Application.Bags.Commands.DeleteBag;
+    using BookShop.Application.Bags.Queries.GetBags;
+    using BookShop.Application.Bags.Queries.GetSingleBag;
     using BookShop.Application.Contracts;
     using BookShop.Application.Games.Commands.CreateGame;
-    using BookShop.Application.Notebooks.Commands.CreateNotebook;
+    using BookShop.Application.Games.Commands.DeleteGame;
+    using BookShop.Application.Games.Queries.GetGames;
+    using BookShop.Application.Games.Queries.GetSingleGame;
     using BookShop.Application.Publications.Commands.CreatePublication;
+    using BookShop.Application.Publications.Commands.DeletePublication;
     using BookShop.Application.Publications.Queries.GetPublication;
     using BookShop.Application.Publications.Queries.GetSinglePublication;
     using BookShop.Application.Users.Commands.CreateApplicatioUser;
     using BookShop.Application.WritingUtensils.Commands.CreateUtensils;
+    using BookShop.Application.WritingUtensils.Commands.DeleteUtensils;
+    using BookShop.Application.WritingUtensils.Queries.GetSingleUtensil;
+    using BookShop.Application.WritingUtensils.Queries.GetUtensils;
     using BookShop.Infrastructure;
 
     using MediatR;
@@ -18,7 +27,6 @@
     {
         public static async Task Main(string[] args)
         {
-            //var editorial = Editorial.Instance;
 
             var diContainer = new ServiceCollection()
                .AddMediatR(typeof(IPublicationRepository))
@@ -30,12 +38,30 @@
                .AddScoped<IApplicationUserRepository, InMemoryApplicationUserRepository>()
                .AddScoped<IGameRepository, InMemoryGameRepository>()
                .AddScoped<IWritingUtensilsRepository, InMemoryWritingUtensilsRepository>()
-               .AddScoped<INotebookRepository, InMemoryNotebookRepository>()
                .AddScoped<IBagRepository, InMemoryBagRepository>()
                .BuildServiceProvider();
 
             var mediator = diContainer.GetRequiredService<IMediator>();
 
+            await PublicationsCommandsAndQueries(mediator);
+
+            await BagCommandsAndQueries(mediator);
+
+            await GameCommandsAndQueries(mediator);
+
+            await UtensilCommandAndQueries(mediator);
+
+            var user = await mediator.Send(new CreateApplicationUserCommand
+            {
+                Id = 1,
+                FirstName = "Gosho",
+                LastName = "Goshov",
+                Email = "Gosho@abv.bg"
+            });
+        }
+     
+        private static async Task PublicationsCommandsAndQueries(IMediator mediator)
+        {
             var publicationId = await mediator.Send(new CreatePublicationCommand
             {
                 Id = 1,
@@ -56,19 +82,11 @@
                 Author = "Grisham",
                 PageCount = 400,
                 Description = "Some description",
-                Genre = new GenreDto{ Name = "Horror" },
+                Genre = new GenreDto { Name = "Horror" },
                 PublicationType = "Book",
             });
 
-            var gameId = await mediator.Send(new CreateGameCommand
-            {
-                Id = 1,
-                Price = 140,
-                Name = "Chess",
-                Manufacturer = "Some carpenter",
-                Description = "Some description",
-                GameType = "Strategy",
-            });
+            Console.WriteLine(await mediator.Send(new DeletePublicationCommand { Id = 2 }));
 
             var publications = await mediator.Send(new GetPublicationsQuery());
             foreach (var publication in publications)
@@ -77,15 +95,10 @@
             }
 
             var book = await mediator.Send(new GetSinglePublicationQuery(1));
+        }
 
-            var user = await mediator.Send(new CreateApplicationUserCommand
-            {
-                Id = 1,
-                FirstName = "Gosho",
-                LastName = "Goshov",
-                Email = "Gosho@abv.bg"
-            });
-
+        private static async Task BagCommandsAndQueries(IMediator mediator)
+        {
             var bagId = await mediator.Send(new CreateBagCommand
             {
                 Id = 1,
@@ -98,26 +111,94 @@
                 BagType = new BagTypeDto { Name = "Big" }
             });
 
-            var notebookId = await mediator.Send(new CreateNotebookCommand
+            var bagId2 = await mediator.Send(new CreateBagCommand
             {
-                Id = 1,
+                Id = 2,
                 Price = 40,
-                Name = "Nike bag",
-                Manufacturer = "Orange",
-                Color = "red",
-                PageCount = 50,
-                LineType = "Squares"
+                Name = "Adidas bag",
+                Manufacturer = "Adidas",
+                Capacity = 50,
+                Color = "white",
+                Gender = "Boy",
+                BagType = new BagTypeDto { Name = "Big" }
             });
 
+            Console.WriteLine(await mediator.Send(new DeleteBagCommand { Id = 2 }));
+
+            var bags = await mediator.Send(new GetBagsQuery());
+            foreach (var currBag in bags)
+            {
+                Console.WriteLine(currBag.Manufacturer);
+            }
+
+            var bag = await mediator.Send(new GetSingleBagQuery(1));
+        }
+
+        private static async Task GameCommandsAndQueries(IMediator mediator)
+        {
+            var gameId = await mediator.Send(new CreateGameCommand
+            {
+                Id = 1,
+                Price = 140,
+                Name = "Chess",
+                Manufacturer = "Some carpenter",
+                Description = "Some description",
+                GameType = "Strategy",
+            });
+
+            var gameId2 = await mediator.Send(new CreateGameCommand
+            {
+                Id = 2,
+                Price = 120,
+                Name = "Chess",
+                Manufacturer = "Some carpenter2",
+                Description = "Some description2",
+                GameType = "Strategy",
+            });
+
+            Console.WriteLine(await mediator.Send(new DeleteGameCommand { Id = 2 }));
+
+            var games = await mediator.Send(new GetGamesQuery());
+            foreach (var currGame in games)
+            {
+                Console.WriteLine(currGame.Name);
+            }
+
+            var game = await mediator.Send(new GetSingleGameQuery(1));
+
+        }
+
+        private static async Task UtensilCommandAndQueries(IMediator mediator)
+        {
             var utensilId = await mediator.Send(new CreateUtensilsCommand
             {
                 Id = 1,
                 Price = 40,
-                Name = "Nike bag",
+                Name = "Nike pen",
                 Manufacturer = "Orange",
                 Color = "red",
                 WritingUtensilsType = new WritingUtensilsTypeDto { Name = "Pen" },
             });
+
+            var utensilId2 = await mediator.Send(new CreateUtensilsCommand
+            {
+                Id = 2,
+                Price = 40,
+                Name = "Adidas pen",
+                Manufacturer = "Orange",
+                Color = "red",
+                WritingUtensilsType = new WritingUtensilsTypeDto { Name = "Pen" },
+            });
+
+            Console.WriteLine(await mediator.Send(new DeleteUtensilCommand { Id = 2 }));
+
+            var utensils = await mediator.Send(new GetUtensilsQuery());
+            foreach (var currUtensil in utensils)
+            {
+                Console.WriteLine(currUtensil.Name);
+            }
+
+            var utensil = await mediator.Send(new GetSingleUtensilQuery(1));
         }
     }
 }
