3480cfce8c4d35e9f53c5c23423e2aa47bcda6d3
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-28 14:20:22 +0200

    Create IDeletableEntityRepository interface

---
+++ BookShop/BookShop.Application/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace BookShop.Application.Repositories
+{
+    using BookShop.Domain.Models;
+
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+       where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
