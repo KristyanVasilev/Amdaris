20d4460bd1c456e6e0d2f58f0903a7565e0c497d
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-30 16:21:56 +0200

    Create UpdateGame Command and Handler

---
+++ BookShop/BookShop.Application/Games/Commands/UpdateGame/UpdateGameCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace BookShop.Application.Games.Commands.UpdateGame
+{
+    using MediatR;
+
+    public class UpdateGameCommand : BaseProductDto, IRequest<int>
+    {
+        public int Id { get; set; }
+
+        public string Manufacturer { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public GenreDto Genre { get; set; } = null!;
+    }
+}
---
+++ BookShop/BookShop.Application/Games/Commands/UpdateGame/UpdateGameHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace BookShop.Application.Games.Commands.UpdateGame
+{
+    using BookShop.Application.Repositories;
+    using BookShop.Domain;
+    using MediatR;
+
+    public class UpdateGameHandler : IRequestHandler<UpdateGameCommand, int>
+    {
+        private readonly IDeletableEntityRepository<Game> repository;
+
+        public UpdateGameHandler(IDeletableEntityRepository<Game> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<int> Handle(UpdateGameCommand request, CancellationToken cancellationToken)
+        {
+            var genre = new Genre { Name = request.Genre.Name };
+
+            var game = this.repository.AllAsNoTracking().FirstOrDefault(x => x.Id == request.Id);
+            if (game == null)
+            {
+                throw new InvalidOperationException("Game cannot be null!");
+            }
+
+            game.Price = request.Price;
+            game.Name = request.Name;
+            game.Description = request.Description;
+            game.Genre = genre;
+
+            this.repository.Update(game);
+            await this.repository.SaveChangesAsync();
+
+            return await Task.FromResult(game.Id);
+        }
+    }
+}
