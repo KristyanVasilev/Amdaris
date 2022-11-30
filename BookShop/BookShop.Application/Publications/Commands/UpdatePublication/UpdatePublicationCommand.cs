b79b2a14c8343f7d9448f7e7e600d8ce98901aba
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-30 12:15:17 +0200

    Create Publication Command and Handler

---
+++ BookShop/BookShop.Application/Publications/Commands/UpdatePublication/UpdatePublicationCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Application.Publications.Commands.UpdatePublication
+{
+    public class UpdatePublicationCommand : BaseProductDto, IRequest<int>
+    {
+        public int Id { get; set; }
+
+        public string Author { get; set; } = null!;
+
+        public int PageCount { get; set; }
+
+        public double Rating { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public string PublicationType { get; set; } = null!;
+
+        public GenreDto Genre { get; set; } = null!;
+    }
+}
---
+++ BookShop/BookShop.Application/Publications/Commands/UpdatePublication/UpdatePublicationHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop.Application.Publications.Commands.UpdatePublication
+{
+    using BookShop.Application.Repositories;
+    using BookShop.Domain;
+    using MediatR;
+
+    public class UpdatePublicationHandler : IRequestHandler<UpdatePublicationCommand, int>
+    {
+        private readonly IDeletableEntityRepository<Publication> repository;
+
+        public UpdatePublicationHandler(IDeletableEntityRepository<Publication> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async  Task<int> Handle(UpdatePublicationCommand request, CancellationToken cancellationToken)
+        {
+
+            var genre = new Genre { Name = request.Genre.Name };
+
+            var publication = this.repository.AllAsNoTracking().FirstOrDefault(x => x.Id == request.Id);
+            if (publication == null)
+            {
+                throw new InvalidOperationException("Publication cannot be null!");
+            }
+
+            publication.Price = request.Price;
+            publication.Name = request.Name;
+            publication.Author = request.Author;
+            publication.PageCount = request.PageCount;
+            publication.Rating = request.Rating;
+            publication.Description = request.Description;
+            publication.Genre = genre;
+            
+            this.repository.Update(publication);
+            await this.repository.SaveChangesAsync();
+
+            return await Task.FromResult(publication.Id);
+        }
+    }
+}
