2b38f2e3e673e78284a11a091f06ea72999565f7
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-11-30 10:50:49 +0200

    Create GetSingleWritingUtensilCommandHandlerTest

---
+++ BookShop/BookShop.Tests/WritingUtensils/Queries/GetSingleWritingUtensilCommandHandlerTest.cs
@@ -0,0 +1,53 @@
+﻿namespace BookShop.Tests.WritingUtensils.Queries
+{
+    using BookShop.Application.Repositories;
+    using BookShop.Application.WritingUtensils;
+    using BookShop.Application.WritingUtensils.Queries.GetSingleUtensil;
+    using BookShop.Domain;
+    using BookShop.Tests.Mocks;
+
+    using Moq;
+    using Shouldly;
+    using Xunit;
+
+    public class GetSingleWritingUtensilCommandHandlerTest
+    {
+        private readonly Mock<IDeletableEntityRepository<WritingUtensil>> mockRepo;
+
+        public GetSingleWritingUtensilCommandHandlerTest()
+        {
+            this.mockRepo = WritingUtensilMockRepository.GetWritingUtensilMockRepo();
+        }
+
+        [Fact]
+        public async Task GetSingleWritingUtensilTest()
+        {
+            var handler = new GetSingleUtensilHandler(this.mockRepo.Object);
+
+            var result = await handler.Handle(new GetSingleUtensilQuery(1), CancellationToken.None);
+
+            result.ShouldBeOfType<WritingUtensilDto>();
+        }
+
+        [Fact]
+        public async Task ShouldntChangeRepoCountTest()
+        {
+            var handler = new GetSingleUtensilHandler(this.mockRepo.Object);
+
+            var result = await handler.Handle(new GetSingleUtensilQuery(1), CancellationToken.None);
+
+            var count = this.mockRepo.Object.All().Count();
+
+            Assert.True(count == 2);
+        }
+
+        [Fact]
+        public async Task ShouldThrowNullReferenceExceptionTest()
+        {
+            var handler = new GetSingleUtensilHandler(this.mockRepo.Object);
+
+            await Assert.ThrowsAsync<NullReferenceException>(() =>
+            handler.Handle(new GetSingleUtensilQuery(12321), CancellationToken.None));
+        }
+    }
+}
