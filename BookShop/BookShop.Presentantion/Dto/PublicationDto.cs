1f06dfe7a7a4bc3e4d070f497f4fd517aad9e75d
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-12-06 19:08:02 +0200

    Create PublicationDto

---
+++ BookShop/BookShop.Presentantion/Dto/PublicationDto.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop.Presentantion.Dto
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PublicationDto
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Author { get; set; } = null!;
+
+        [Range(0.5, int.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Range(5, int.MaxValue)]
+        public int PageCount { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 10)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string PublicationType { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Genre { get; set; } = null!;
+    }
+}
