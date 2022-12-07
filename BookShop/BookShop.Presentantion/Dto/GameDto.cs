145409a4fd019b356a8c7b5754a1500c538b0623
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-12-07 14:31:44 +0200

    Create GameDto

---
+++ BookShop/BookShop.Presentantion/Dto/GameDto.cs
@@ -0,0 +1,26 @@
+﻿namespace BookShop.Presentantion.Dto
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class GameDto
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Manufacturer { get; set; } = null!;
+
+        [Range(0.5, int.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 10)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Genre { get; set; } = null!;
+    }
+}
