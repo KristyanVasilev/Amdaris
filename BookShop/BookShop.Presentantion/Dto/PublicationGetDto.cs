17b485f687498c7b7b72d7daf25ca6c5419f235c
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-12-12 15:58:10 +0200

    Create PublicationGetDto

---
+++ BookShop/BookShop.Presentantion/Dto/PublicationGetDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Presentantion.Dto
+{
+    public class PublicationGetDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+    }
+}
