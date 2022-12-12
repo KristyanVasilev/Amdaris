fe45d5b7d2388eb436e50e02a596a0174b1fdc3a
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-12-12 15:57:53 +0200

    Create PublicationProfile

---
+++ BookShop/BookShop.Presentantion/Profiles/PublicationProfile.cs
@@ -0,0 +1,17 @@
+﻿namespace BookShop.Presentantion.Profiles
+{
+    using AutoMapper;
+    using BookShop.Application.Publications;
+    using BookShop.Domain;
+    using BookShop.Presentantion.Dto;
+
+    public class PublicationProfile : Profile
+    {
+        public PublicationProfile()
+        {
+            CreateMap<Publication, PublicationGetDto>();
+            CreateMap<PublicationGetDto, Publication>();
+
+        }
+    }
+}
--- BookShop/BookShop.Presentantion/Program.cs
+++ BookShop/BookShop.Presentantion/Program.cs
@@ -2,6 +2,7 @@ using BookShop.Application.Publications.Commands.CreatePublication;
 using BookShop.Application.Repositories;
 using BookShop.Infrastructure;
 using BookShop.Infrastructure.Repositories;
+using BookShop.Presentantion;
 using MediatR;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
@@ -26,25 +27,25 @@ builder.Services.AddMediatR(typeof(CreatePublicationCommand));
 builder.Services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
 builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
 
-// Auto Mapper
-//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// AutoMapper
+builder.Services.AddAutoMapper(typeof(AssemblyMarketPresentatio));
 
 var app = builder.Build();
 
-app.UseExceptionHandler(
-    options =>
-    {
-        options.Run(
-            async contex =>
-            {
-                contex.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                var ex = contex.Features.Get<IExceptionHandlerFeature>();
-                if (ex != null)
-                {
-                    await contex.Response.WriteAsync(ex.Error.Message);
-                }
-            });
-    });
+//app.UseExceptionHandler(
+//    options =>
+//    {
+//        options.Run(
+//            async contex =>
+//            {
+//                contex.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+//                var ex = contex.Features.Get<IExceptionHandlerFeature>();
+//                if (ex != null)
+//                {
+//                    await contex.Response.WriteAsync(ex.Error.Message);
+//                }
+//            });
+//    });
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
