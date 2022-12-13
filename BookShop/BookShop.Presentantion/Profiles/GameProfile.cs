b5603c418745ef946deb478b7d4390dfabc945e5
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-12-13 13:08:08 +0200

    Create GameProfile

---
+++ BookShop/BookShop.Presentantion/Profiles/GameProfile.cs
@@ -0,0 +1,17 @@
+﻿namespace BookShop.Presentantion.Profiles
+{
+    using AutoMapper;
+    using BookShop.Presentantion.Dto;
+    using BookShop.Application.Games;
+    using BookShop.Application.Games.Commands.CreateGame;
+
+    public class GameProfile : Profile
+    {
+        public GameProfile()
+        {
+            CreateMap<GameDto, GameGetDto>();
+
+            CreateMap<GamePostDto, CreateGameCommand>();
+        }
+    }
+}
