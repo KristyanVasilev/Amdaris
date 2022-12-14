ff9aa6fcec2f166a418fad8faa4461e274624de5
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-12-14 17:57:38 +0200

    Create UserController

---
+++ BookShop/BookShop.Presentantion/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using BookShop.Domain;
+using BookShop.Presentantion.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Presentantion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public UserController(
+            UserManager<ApplicationUser> _userManager,
+            SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody] RegisterDto model)
+        {
+
+            var user = new ApplicationUser()
+            {
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                UserName = model.UserName,
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+
+            return Ok(model);
+        }
+    }
+}
