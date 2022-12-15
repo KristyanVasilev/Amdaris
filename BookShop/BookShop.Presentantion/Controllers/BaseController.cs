31601a754ffef5e670cf87f75805433b5c3abf2f
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-12-15 15:38:26 +0200

    Create BaseController

---
+++ BookShop/BookShop.Presentantion/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿namespace BookShop.Presentantion.Controllers
+{
+    using AutoMapper;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BaseController<T> : ControllerBase where T : BaseController<T>
+    {
+        private ILogger<T>? logger;
+        private IMapper? mapper;
+        private IMediator? mediator;
+
+        protected ILogger<T> Logger
+            => this.logger ??= HttpContext.RequestServices.GetRequiredService<ILogger<T>>();
+
+        protected IMapper Mapper
+            => this.mapper ??= HttpContext.RequestServices.GetRequiredService<IMapper>();
+
+        protected IMediator Mediator
+            => this.mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+    }
+}
