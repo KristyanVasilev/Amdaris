a70843ce3d4b2a53bde3c9e4338c1e85c63cd1bf
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-12-19 16:14:16 +0200

    Create BlobStorageService

---
+++ BookShop/BookShop.Infrastructure/ThirdPartyServices/AzureServices/BlobStorageService.cs
@@ -0,0 +1,42 @@
+﻿namespace BookShop.Infrastructure.ThirdPartyServices.AzureServices
+{
+    using Azure.Storage.Blobs;
+    using Azure.Storage.Blobs.Models;
+    using BookShop.Application.Images.DTOs;
+    using BookShop.Application.Images.Interfaces;
+
+    public class BlobStorageService : IFileStorageService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BlobStorageService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public async Task<UrlDto> UploadAsync(ICollection<FileDto> files)
+        {
+            if (files == null || files.Count == 0)
+            {
+                return null;
+            }
+
+            var containerClient = _blobServiceClient.GetBlobContainerClient("publicupload");
+
+
+            var urls = new List<string>();
+
+
+            foreach (var file in files)
+            {
+                var blobClient = containerClient.GetBlobClient(file.GetPathWithFileName());
+
+                await blobClient.UploadAsync(file.Content, new BlobHttpHeaders { ContentType = file.ContentType });
+
+                urls.Add(blobClient.Uri.ToString());
+            }
+
+            return new UrlDto(urls);
+        }
+    }
+}
