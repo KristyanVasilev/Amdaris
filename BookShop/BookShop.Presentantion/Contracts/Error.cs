82dd36f37679bd9ff937008a9b4c3649373e6254
KristyanVasilev <kristiyanvasilev02@gmail.com>
2022-12-21 15:57:08 +0200

    Create Error class

---
+++ BookShop/BookShop.Presentantion/Contracts/Error.cs
@@ -0,0 +1,18 @@
+﻿namespace BookShop.Presentantion.Contracts
+{
+    using Newtonsoft.Json;
+
+    public class Error
+    {
+        public string? StatusCode { get; set; }
+
+        public ICollection<string>? Message { get; set; } = new List<string>();
+
+        public DateTime? TimeSpan { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
