6ee884448fcb3b1269bda02397b7dcb9d876f0e1
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-01-09 17:03:46 +0200

    Update header component

--- BookShopFrontEnd/src/app/services/cart.service.ts
+++ BookShopFrontEnd/src/app/services/cart.service.ts
@@ -25,7 +25,7 @@ export class CartService {
     this._snackBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
     console.log(this.cart.value);
   }
-  
+
   clearCart(): void {
     this.cart.next({ items: [] });
     this._snackBar.open('Cart is cleared.', 'Ok', {
@@ -37,5 +37,44 @@ export class CartService {
     return items
       .map((item) => item.price * item.quantity)
       .reduce((prev, current) => prev + current, 0);
+  }
+
+  removeFromCart(item: CartItem, updateCart = true): CartItem[] {
+    const filteredItems = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id
+    );
+
+    if (updateCart) {
+      this.cart.next({ items: filteredItems });
+      this._snackBar.open('1 item removed from cart.', 'Ok', {
+        duration: 3000,
+      });
+    }
+
+    return filteredItems;
+  }
+
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval!: CartItem;
+
+    let filteredItems = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id) {
+        _item.quantity--;
+        if (_item.quantity === 0) {
+          itemForRemoval = _item;
+        }
+      }
+
+      return _item;
+    });
+
+    if (itemForRemoval) {
+      filteredItems = this.removeFromCart(itemForRemoval, false);
+    }
+
+    this.cart.next({ items: filteredItems });
+    this._snackBar.open('1 item removed from cart.', 'Ok', {
+      duration: 3000,
+    });
   }
 }
