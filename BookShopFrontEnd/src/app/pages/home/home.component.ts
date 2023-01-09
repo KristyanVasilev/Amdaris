899e60e6045ea11ca593a96f20ad72d29ed7f251
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-01-09 15:28:50 +0200

    Update home Component

--- BookShopFrontEnd/src/app/pages/home/home.component.ts
+++ BookShopFrontEnd/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 
@@ -12,6 +13,10 @@ export class HomeComponent {
   category: string | undefined;
   rowHeight: number = ROWS_HEIGHT[this.cols];
 
+  constructor(private cartService: CartService){
+    
+  }
+
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
     this.rowHeight = ROWS_HEIGHT[colsNum];
@@ -19,5 +24,15 @@ export class HomeComponent {
 
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
+  }
+
+  onAddToCart(product: any): void {
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id,
+    });
   }
 }
