740bcf7fe6a9a43123513821f3b66f308b02c063
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-01-09 12:26:01 +0200

    Update home Component

--- BookShopFrontEnd/src/app/pages/home/home.component.ts
+++ BookShopFrontEnd/src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,12 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   cols = 3;
-  //rowHeight: number = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
+  rowHeight: number = ROWS_HEIGHT[this.cols];
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
-    //this.rowHeight = ROWS_HEIGHT[colsNum];
-    console.log(this.cols)
+    this.rowHeight = ROWS_HEIGHT[colsNum];
   }
 
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
 }
