81dc9b965b60bcd9b976fc99986808b2f6d4a8c6
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-01-10 17:05:27 +0200

    Update filters component

--- BookShopFrontEnd/src/app/pages/home/components/filters/filters.component.ts
+++ BookShopFrontEnd/src/app/pages/home/components/filters/filters.component.ts
@@ -9,6 +9,7 @@ export class FiltersComponent {
   @Output() showCategory = new EventEmitter<string>();
 
   categories = ['all', 'publications', 'games', 'writing utensils']
+  items = ['publication', 'game', 'writing utensil']
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category);
