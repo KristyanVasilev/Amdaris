20734ce7f35c3dc20008743beb6ea150f7aaa6f6
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-01-10 13:30:28 +0200

    Update Home Component

--- BookShopFrontEnd/src/app/pages/home/home.component.ts
+++ BookShopFrontEnd/src/app/pages/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models/game';
+import { Publication } from 'src/app/models/publication';
 import { CartService } from 'src/app/services/cart.service';
+import { GameService } from 'src/app/services/game.service';
+import { PublicationService } from 'src/app/services/publication.service';
 
 const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 
@@ -12,9 +17,34 @@ export class HomeComponent {
   cols = 3;
   category: string | undefined;
   rowHeight: number = ROWS_HEIGHT[this.cols];
+  games: Array<Game> | undefined;
+  publications: Array<Publication> | undefined;
+  gamesSubsription: Subscription | undefined;
+  publicationsSubsription: Subscription | undefined;
 
-  constructor(private cartService: CartService){
-    
+  constructor(
+    private cartService: CartService,
+    private gameService: GameService,
+    private publicationService: PublicationService) {
+
+    this.getGames();
+    this.getPublications();
+  }
+
+  getGames(): void {
+    this.gamesSubsription = this.gameService
+      .getAllGames()
+      .subscribe((_games) => {
+        this.games = _games;
+      });
+  }
+
+  getPublications(): void {
+    this.publicationsSubsription = this.publicationService
+      .getAllPublications()
+      .subscribe((_publications) => {
+        this.publications = _publications;
+      });
   }
 
   onColumnsCountChange(colsNum: number): void {
@@ -34,5 +64,15 @@ export class HomeComponent {
       quantity: 1,
       id: product.id,
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.gamesSubsription) {
+      this.gamesSubsription.unsubscribe();
+    }
+
+    if (this.publicationsSubsription) {
+      this.publicationsSubsription.unsubscribe();
+    }
   }
 }
