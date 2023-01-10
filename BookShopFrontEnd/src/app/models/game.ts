425338a99fc5ac57d2113e8a424c8624f1fba2e0
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-01-10 13:28:35 +0200

    Create Publication and Game interface

---
+++ BookShopFrontEnd/src/app/models/game.ts
@@ -0,0 +1,9 @@
+export interface Game {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    genre: string;
+    manufacturer: string;
+    images: string[];
+}
---
+++ BookShopFrontEnd/src/app/models/publication.ts
@@ -0,0 +1,9 @@
+export interface Publication {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    genre: string;
+    author: string;
+    images: string[];
+}
