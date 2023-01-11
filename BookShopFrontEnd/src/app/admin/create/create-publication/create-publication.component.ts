3f0b4a82bcac0c9b76d65c93eea79e81b3219a2a
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-01-11 14:29:44 +0200

    Create create-publication component

---
+++ BookShopFrontEnd/src/app/admin/create/create-publication/create-publication.component.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Publication } from 'src/app/models/publication';
+import { PublicationService } from 'src/app/services/publication.service';
+
+@Component({
+  selector: 'app-create-publication',
+  templateUrl: './create-publication.component.html',
+  styleUrls: ['./create-publication.component.css']
+})
+export class CreatePublicationComponent {
+  publication: Publication = {
+    id: 0,
+    name: '',
+    author: '',
+    price: 0,
+    pageCount: 0,
+    description: '',
+    genre: '',
+    images: [] as string[]
+  };
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private publicationService: PublicationService) { }
+
+  shortLink: string = "";
+
+  loading: boolean = false; // Flag variable
+
+  file?: File; // Variable to store file
+  upload(file: any): Observable<any> {
+    const formData = new FormData();
+    formData.append("file", file, file.name);
+    return this.http.post('https://localhost:7201/api/Files/Images', formData)
+  }
+
+  onChange(event: any) {
+
+    this.file = event.target.files[0];
+  }
+
+  onUpload() {
+
+    this.loading = !this.loading;
+    console.log(this.file);
+
+    this.upload(this.file).subscribe(
+      (event: any) => {
+        console.log(event);
+        console.log(JSON.stringify(event).substring(10, JSON.stringify(event).length - 3));
+        this.publication.images.push(JSON.stringify(event).substring(10, JSON.stringify(event).length - 3));
+        if (typeof (event) === 'object') {
+          this.shortLink = event.link;
+          this.loading = false; // Flag variable
+        }
+      }
+    );
+  }
+
+  createPublication() {
+    this.publicationService.createPublication(this.publication).subscribe(res => console.log(res));
+    this.router.navigate(['home'])
+  }
+}
