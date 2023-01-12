343fb71df438caeab56f80a0bf53295e8607521e
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-01-12 16:54:56 +0200

    Update HttpErrorInterceptor

--- BookShopFrontEnd/src/app/error-handling/HttpErrorInterceptor.ts
+++ BookShopFrontEnd/src/app/error-handling/HttpErrorInterceptor.ts
@@ -14,13 +14,13 @@ export class HttpErrorInterceptor {
       .pipe(
         catchError((error: HttpErrorResponse) => {
           if (error.status === 400) {
-            console.log(error.message, "Bad Request")
+            console.log("Bad Request!", error.message)
           }
           else if (error.status === 404) {
-            console.log("Not Found!")
+            console.log("Not Found!" , error.message)
           }
           else if (error.status === 500) {
-            console.log("Iternal error!")
+            console.log("Iternal error!" , error.message)
             //redirect to smth went wrong
           }
           return throwError(error);
