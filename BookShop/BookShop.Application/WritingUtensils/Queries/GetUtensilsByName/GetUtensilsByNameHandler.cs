0b8910c446b6870bae8c4384b2865373ebab1ef6
KristyanVasilev <kristiyanvasilev02@gmail.com>
2023-01-16 16:32:16 +0200

    Rename GetUtensilsByType to GetUtensilsByName command, handler and controller method

---
+++ BookShop/BookShop.Application/WritingUtensils/Queries/GetUtensilsByName/GetUtensilsByNameHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace BookShop.Application.WritingUtensils.Queries.GetUtensilsByName
+{
+    using BookShop.Application.Repositories;
+    using BookShop.Application.SeedWork.Exceptions;
+    using BookShop.Domain;
+    using MediatR;
+
+    public class GetUtensilsByNameHandler : IRequestHandler<GetUtensilsByNameQuery, WritingUtensilDto>
+    {
+        private readonly IDeletableEntityRepository<WritingUtensil> repository;
+        private readonly IRepository<WritingUtensilsType> utensilRepository;
+
+        public GetUtensilsByNameHandler(
+            IDeletableEntityRepository<WritingUtensil> repository,
+            IRepository<WritingUtensilsType> utensilRepository)
+        {
+            this.repository = repository;
+            this.utensilRepository = utensilRepository;
+        }
+
+        public async Task<WritingUtensilDto> Handle(GetUtensilsByNameQuery request, CancellationToken cancellationToken)
+        {
+            var utensil = this.repository
+                              .AllAsNoTracking()
+                              .FirstOrDefault(x => x.Name == request.Name)
+                              ?? throw new WritingUtensilNotFoundException("Utensil not found!");
+
+            var type = this.utensilRepository
+                         .AllAsNoTracking()
+                         .FirstOrDefault(t => t.Id == utensil.WritingUtensilsTypeId);
+
+            var result = new WritingUtensilDto
+            {
+                Id = utensil.Id,
+                Name = utensil.Name,
+                Price = utensil.Price,
+                Color = utensil.Color,
+                Manufacturer = utensil.Manufacturer,
+                Images = utensil.Images,
+                WritingUtensilsType = type?.Name ?? "No type",
+            };
+
+            return await Task.FromResult(result);
+        }
+    }
+}
---
+++ BookShop/BookShop.Application/WritingUtensils/Queries/GetUtensilsByName/GetUtensilsByNameQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace BookShop.Application.WritingUtensils.Queries.GetUtensilsByName
+{
+    using MediatR;
+
+    public class GetUtensilsByNameQuery : IRequest<WritingUtensilDto>
+    {
+        public GetUtensilsByNameQuery(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
